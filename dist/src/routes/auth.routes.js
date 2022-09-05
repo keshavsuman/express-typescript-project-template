b716ff5ff5118813c3771a51b94ac79f3fd2c707
Keshav suman <keshavsuman96@gmail.com>
2022-09-05 14:44:31 +0530

    build command added

--- dist/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const router = (0, express_1.Router)();
-router.post("/signup", controllers_1.AuthenticationController.signup);
-router.post("/login", controllers_1.AuthenticationController.login);
-exports.default = router;
